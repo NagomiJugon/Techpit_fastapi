a3775338aae8dfaed852e09c9237f3f739b61eff
f sato <fsato@test.com>
2025-12-30 06:18:14 +0900

    chore: include remaining UI/dark-mode and layout fixes

--- front/app/broccoli-front/src/components/Content/Content.tsx
+++ front/app/broccoli-front/src/components/Content/Content.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 
 export const Content = ({ children }: LayoutProps) => {
     return (
-        <div className="w-full lg:ps-64">
+        <div className="w-full">
             <div className="p-4 sm:p-6 space-y-4 sm:space-y-6">
                 {children}
             </div>
